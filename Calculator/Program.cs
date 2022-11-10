83b040274391ccc2fa4ff74293dca52776869f2d
Rennan Alves <rennan_areas@hotmail.com>
2022-11-10 13:45:27 -0300

    Create functions

---
+++ Calculator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+            Console.WriteLine();
+            Console.WriteLine($"Resultado: {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+            Console.WriteLine();
+            Console.WriteLine($"Resultado: {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 / v2;
+            Console.WriteLine();
+            Console.WriteLine($"Resultado: {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.Write("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 * v2;
+            Console.WriteLine();
+            Console.WriteLine($"Resultado: {resultado}");
+            Console.ReadKey();
+        }
+    }
+}
